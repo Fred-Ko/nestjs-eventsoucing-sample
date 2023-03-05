a5543ac736b7d83be9e953f12f91a1ebbcc458d8
Fred-Ko <fredko328@gmail.com>
2023-03-06 01:56:49 +0900

    각종 설정파일 추가

---
+++ src/common/common.module.ts
@@ -0,0 +1,19 @@
+﻿import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AllExceptionsFilter } from '@src/common/exception-ilters/http.filter';
+import { EventModule } from '@src/event/event.module';
+import { EventStoreModule } from '@src/event-store/event.store.module';
+import { UserModule } from '@src/user/user.module';
+import { PubSub } from 'graphql-subscriptions';
+import { ClsModule } from 'nestjs-cls';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class ConfigModule {}
