4719f2ce0e100d3a1f1a693375f7a25d08b31ab1
Fred-Ko <fredko328@gmail.com>
2023-03-06 03:09:51 +0900

    환경변수 관련 유틸 함수 추가

---
+++ util.dotenv2json.py
@@ -0,0 +1,14 @@
+﻿import json
+
+with open('.env', 'r') as f:
+    # read .env file and create dictionary object
+    data = {}
+    for line in f:
+        line = line.strip()
+        if line and not line.startswith('#'):
+            key, value = line.split('=', 1)
+            data[key] = value
+
+with open('.env.json', 'w') as f:
+    # write dictionary object to env.json file
+    json.dump(data, f, indent=2)
---
+++ util.yaml2dotenv.py
@@ -0,0 +1,35 @@
+﻿import yaml
+
+out=''
+
+def write_env_variables(f, data, section_name='',path=''):  
+    global out
+    path = f'{path}//{section_name}'
+
+    split_path = path[4:].split('//')
+    out += ('\n')
+    for i in range(len(split_path)):
+        out += f'# {"  "*i}{split_path[i].split("_")[-1]}\n'
+
+    for key, value in data.items():
+        if isinstance(value, dict):
+            # nested section
+            section_name = f"{section_name}_{key.upper()}" if section_name else key.upper()
+            write_env_variables(f, value, section_name,path)
+        else:
+            # leaf node
+            env_var_name = f"{section_name.upper()}_{key.upper()}"
+            if isinstance(value, bool):
+                env_var_value = str(value).lower()
+            else:
+                env_var_value = str(value)
+            out += (f"{env_var_name}={env_var_value}\n")        
+
+with open('./.env.yaml', 'r') as f:
+    data = yaml.safe_load(f)
+
+with open('./.env', 'w') as f:
+    write_env_variables(f, data)
+    out_lines = out.split('\n')
+    out = '\n'.join(out_lines[8:])
+    f.write(out)
