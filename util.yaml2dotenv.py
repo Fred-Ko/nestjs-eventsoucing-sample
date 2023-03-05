448743822f4f7bc9b8e3d1d2fd499e29e7f036c0
Fred-Ko <fredko328@gmail.com>
2023-03-06 04:21:11 +0900

    yaml2dotenv 로직 버그 수정

--- util.yaml2dotenv.py
+++ util.yaml2dotenv.py
@@ -1,21 +1,52 @@
 ﻿import yaml
+from collections import OrderedDict
+
+class OrderedSet:
+    def __init__(self, items=None):
+        self.dict = OrderedDict()
+        if items is not None:
+            for item in items:
+                self.add(item)
+
+    def __len__(self):
+        return len(self.dict)
+
+    def __contains__(self, item):
+        return item in self.dict
+
+    def add(self, item):
+        if item == '':
+            return
+        self.dict[item] = None
+
+    def remove(self, item):
+        del self.dict[item]
+
+    def __iter__(self):
+        return iter(self.dict)
 
 out=''
+example=''
 
-def write_env_variables(f, data, section_name='',path=''):  
-    global out
-    path = f'{path}//{section_name}'
+def write_env_variables(f, data, section_name='',path=OrderedSet()):
+    global out,example
+    path.add(section_name.split('_')[-1])
 
-    split_path = path[4:].split('//')
+    split_path = list(path)
     out += ('\n')
-    for i in range(len(split_path)):
+    example += ('\n')
+
+    for i,v in enumerate(list(path)):
         out += f'# {"  "*i}{split_path[i].split("_")[-1]}\n'
+        example += f'# {"  "*i}{split_path[i].split("_")[-1]}\n'
+
+    basekey = "_".join(list(path))
 
     for key, value in data.items():
         if isinstance(value, dict):
             # nested section
-            section_name = f"{section_name}_{key.upper()}" if section_name else key.upper()
-            write_env_variables(f, value, section_name,path)
+            section_name = f"{basekey.upper()}_{key.upper()}" if section_name else key.upper()
+            write_env_variables(f, value, section_name,OrderedSet(path))
         else:
             # leaf node
             env_var_name = f"{section_name.upper()}_{key.upper()}"
@@ -23,13 +54,19 @@ def write_env_variables(f, data, section_name='',path=''):
                 env_var_value = str(value).lower()
             else:
                 env_var_value = str(value)
-            out += (f"{env_var_name}={env_var_value}\n")        
+            out += (f"{env_var_name}={env_var_value}\n")
+            example += (f"{env_var_name}=\n")
 
 with open('./.env.yaml', 'r') as f:
     data = yaml.safe_load(f)
 
+write_env_variables(f, data)
 with open('./.env', 'w') as f:
-    write_env_variables(f, data)
     out_lines = out.split('\n')
-    out = '\n'.join(out_lines[8:])
+    out = '\n'.join(out_lines[4:])
+    f.write(out)
+
+with open('./.env.example', 'w') as f:
+    out_lines = example.split('\n')
+    out = '\n'.join(out_lines[4:])
     f.write(out)
